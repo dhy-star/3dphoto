1093f81e01e25f5905a53c54320af731e8220839
theD <763036679@qq.com>
2019-11-26 14:54:17 +0800

    3d相册0.0.1

---
+++ src/js/imges.js
@@ -0,0 +1,44 @@
+window.onload=function(){
+    let imgAll = document.getElementsByTagName('img')
+    let imgAllLength = imgAll.length
+    let deg = 360/imgAllLength
+    let warp = document.getElementsByClassName('warp')[0]
+    let startx = 0
+    let starty = 0
+    let x = 0
+    let y = 0
+    let endx = 0
+    let endy = 0
+    //warp.style.transform =`rotateX(${x}deg) rotateY(${y}deg)`
+   // console.log(deg);
+    Array.prototype.forEach.call(imgAll,function (ele,index) {
+        ele.style.transform = `rotateY(${deg*index}deg) translateZ(${350}px)`
+        ele.style.transition =`${1}s ${(imgAllLength-index)*0.1}s`
+    })
+    document.onmousedown = function (e) {
+
+         startx = e.pageX*0.1
+        starty = e.pageY*0.1
+
+
+        console.log(startx + '**' + starty);
+        document.onmousemove = function (e) {
+
+            let overx = e.pageX*0.1
+            let overy = e.pageY*0.1
+            endx = overx - startx
+             endy = -(overy - starty)
+            x = endx + x
+            y = endy + y
+            warp.style.transform =`rotateY(${x}deg) rotateX(${y}deg)`
+
+        }
+        document.onmouseup = function () {
+            //warp.style.transform =`rotateY(${x}deg) rotateX(${y}deg)`
+            document.onmousemove = null
+
+           // console.log('松开了');
+        }
+    }
+}
+
